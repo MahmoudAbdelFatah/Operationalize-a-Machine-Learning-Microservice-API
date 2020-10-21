22e7286796ef2798c941d41b8c1ef2b56bad96dc
EC2 Default User <ec2-user@ip-172-31-29-45.us-east-2.compute.internal>
2020-10-21 01:48:54 +0000

    the 4th project in Cloud DevOps Nanodegree on Udacity

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="mahmoudteha/api"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag api:latest mahmoudteha/api:latest
+docker login --username=mahmoudteha
 
 # Step 3:
 # Push image to a docker repository
+docker push mahmoudteha/api:latest
